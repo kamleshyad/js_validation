0d9999782776d3a5fb6d97bd1c71947bae7cca57
kamleshyad <kamleshyadav1795@gmail.com>
2023-09-10 17:57:50 +0530

    css done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-alert("Hello Kamlesh");
+let myname="    kamlesh";
+console.log(myname);
+console.log(myname.trim());
+let fruit = "asple";
+console.log(fruit.charAt(0))
+console.log(fruit.slice(1,2))
